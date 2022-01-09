4777e392dcf0362cfbcdf94161a455329cb0db32
Nick <soehner.nick@gmail.com>
2022-01-09 14:42:20 -0700

    quite a few additions

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Landing
 let grayClouds = document.getElementById("gray-clouds");
 let rocket = document.getElementById("rocket");
 let lightClouds = document.getElementById("light-clouds");
@@ -9,3 +10,22 @@ window.addEventListener("scroll", function () {
     grayClouds.style.top = value * -0.1 + "px";
     lightClouds.style.top = value * -0.001 + "px";
 });
+
+// ScrollMagic
+let controller = new ScrollMagic.Controller();
+
+let skillsScene = new ScrollMagic.Scene({
+    triggerElement: "#skills",
+    duration: "100%",
+    triggerHook: 0.4,
+})
+.setClassToggle(".box", "show")
+.addTo(controller);
+
+let aboutScene = new ScrollMagic.Scene({
+    triggerElement: '#about-me',
+    triggerHook: 0.4,
+})
+    .setClassToggle(".headshot, .about-text", "visable")
+    .addTo(controller);
+
