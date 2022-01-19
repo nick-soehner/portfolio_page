cf96363bed279c5dcacbe5161a0e77590d46d5e6
Nick <soehner.nick@gmail.com>
2022-01-19 15:30:43 -0700

    added mobile menu

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+//Menu
+const menu = document.getElementById("menu");
+const navbar = document.getElementById("mobile-nav");
+
+menu.addEventListener("click", function () {
+    if (navbar.style.display === "none") {
+        navbar.style.display = "block";
+    } else {
+        navbar.style.display = "none";
+    }
+});
+
 //Landing
 let grayClouds = document.getElementById("gray-clouds");
 let rocket = document.getElementById("rocket");
@@ -5,6 +17,7 @@ let lightClouds = document.getElementById("light-clouds");
 let whiteClouds = document.getElementById("white-clouds");
 
 window.addEventListener("scroll", function () {
+    navbar.style.display = "none";
     let value = window.scrollY;
     rocket.style.top = value * -0.8 + "px";
     grayClouds.style.top = value * -0.1 + "px";
@@ -18,13 +31,12 @@ let skillsScene = new ScrollMagic.Scene({
     triggerElement: "#skills",
     triggerHook: 0.4,
 })
-.setClassToggle(".box", "show")
-.addTo(controller);
+    .setClassToggle(".box", "show")
+    .addTo(controller);
 
 let aboutScene = new ScrollMagic.Scene({
-    triggerElement: '#about-me',
+    triggerElement: "#about-me",
     triggerHook: 0.4,
 })
     .setClassToggle(".headshot, .about-text", "visable")
     .addTo(controller);
-
